9ae46b6a9c60c202747dd6e013972fac62f569c9
unknown <ITSYADM@GORI13.itsy.edu.ec>
2018-07-09 15:33:57 -0500

    creacion de nuevas tablas

--- BDD/Base de Datos PI.sql
+++ BDD/Base de Datos PI.sql
@@ -9,13 +9,20 @@ apellido varchar (50) not null,
 correo varchar (100) not null,
 primary key (id));
 
-Create table Ubicacion (
+Create table UbicacionMuseo (
 id int not null auto_increment,
-ubicacion1 varchar (100) not null,
-ubicacion  varchar (100) not null,
-ubicacion  varchar (100) not null,
-ubicacion  varchar (100) not null,
-ubicacion  varchar (100) not null,
-ubicacion  varchar (100) not null,
+nombre varchar (100) not null,
+longuitud  varchar (100) not null,
+latitud  varchar (100) not null,
 primary key (id));
 
+Create table Ruta (
+id int not null auto_increment,
+direccion varchar(100) not null,
+primary key (id));
+
+Create table Cliente (
+id int not null auto_increment,
+coordenadaActual int null,
+idPersona int null,
+primary key (id));
